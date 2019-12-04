53c3a6e753c4cb0ff2fbd24fab235c54d7cc57c8
cherrykot <kot.aleksei43@gmail.com>
2019-12-04 03:01:40 +0200

    HW-603: + sorting for todo items

--- src/components/main/todolist/todolist.js
+++ src/components/main/todolist/todolist.js
@@ -3,24 +3,23 @@ import PropTypes from 'prop-types';
 import TodoItem from './todoitem';
 import './style.css';
 
-function TodoList (props) {
+function TodoList(props) {
   const { todolist, editTodo, toggleDone, deleteTodo } = props;
   return (
-    <ul className='todoList'>
-      {
-        todolist.map((todo) => {
-          return (
-            <TodoItem 
-              key={todo.id} 
-              todo={todo}
-              editTodo={editTodo}
-              toggleDone={toggleDone}
-              deleteTodo={deleteTodo} />
-          );
-        })
-      }
+    <ul className="todoList">
+      {todolist.map(todo => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            editTodo={editTodo}
+            toggleDone={toggleDone}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
     </ul>
-  )
+  );
 }
 
 TodoList.propTypes = {
@@ -30,11 +29,12 @@ TodoList.propTypes = {
       id: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
       description: PropTypes.string.isRequired,
-      priority: PropTypes.string.isRequired
-  })).isRequired,
+      priority: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
   editTodo: PropTypes.func.isRequired,
   toggleDone: PropTypes.func.isRequired,
-  deleteTodo: PropTypes.func.isRequired
+  deleteTodo: PropTypes.func.isRequired,
 };
 
 export default TodoList;
