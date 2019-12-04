dababb43436711e11c0e0037ad027d62448b2c4f
cherrykot <kot.aleksei43@gmail.com>
2019-12-04 12:46:44 +0200

    + routing to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './app.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './header';
 import Footer from './footer';
 import Main from './main/main';
 
-function App() {
+function TodosPage() {
   return (
     <>
-      <Header/>
+      <Header />
       <Main />
       <Footer />
     </>
+  );
+}
+
+function NotFoundPage() {
+  return <h1>Not Found...</h1>;
+}
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/todos" />
+      </Route>
+      <Route path="/todos" component={TodosPage} />
+      <Route path="*" component={NotFoundPage} />
+    </Switch>
   );
 }
 
