d5e3b32f2b79a6694b57609109f6450da91d44e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-04 10:13:29 +0000

    Deploy KotAleksei/js-band-hw-task-6 to github.com/KotAleksei/js-band-hw-task-6.git:gh-pages

--- precache-manifest.f4462872c27a39ca5736a9c9cdf84323.js
+++ precache-manifest.f4462872c27a39ca5736a9c9cdf84323.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "956b1de80ce4c102468cefd5fe1f24e9",
+    "revision": "37b8ccce1a8727c7771b7d6045a0f47d",
     "url": "/js-band-hw-task-6/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js-band-hw-task-6/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "a0db065f47f8c1761e9d",
-    "url": "/js-band-hw-task-6/static/css/main.8368567a.chunk.css"
+    "revision": "b0c31d0423aaca4702cb",
+    "url": "/js-band-hw-task-6/static/css/main.bcac620f.chunk.css"
   },
   {
     "revision": "0a2e3e04a83332fae11c",
     "url": "/js-band-hw-task-6/static/js/2.491c3fdb.chunk.js"
   },
   {
-    "revision": "a0db065f47f8c1761e9d",
-    "url": "/js-band-hw-task-6/static/js/main.4617178a.chunk.js"
+    "revision": "b0c31d0423aaca4702cb",
+    "url": "/js-band-hw-task-6/static/js/main.1d8864b0.chunk.js"
   },
   {
     "revision": "ba09b9f3d2cd2824722f",
